e94ec145957934e47e0616046eb2a2272519ba30
Jennica Egan <jennica@jackrussellsoftware.com>
2011-06-22 16:48:37 -0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Blog::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
   
-  get "pages/about"
-
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
