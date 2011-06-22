3237682ce127b7e08ea1bcff6efafda61d3d7bcc
Jennica Egan <jennica@jackrussellsoftware.com>
2011-06-22 17:09:24 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Blog::Application.routes.draw do
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
